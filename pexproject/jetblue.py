3673585a6cf91f7d7401aca950ff9e0d59b02b9c
it-coral <coral1212@yahoo.com>
2016-02-10 17:16:25 +0530

    add virgin america scraper

--- pexproject/jetblue.py
+++ pexproject/jetblue.py
@@ -31,6 +31,8 @@ def jetblue(from_airport,to_airport,searchdate,searchid):
     dt = datetime.datetime.strptime(searchdate, '%Y/%m/%d')
     date = dt.strftime('%d-%m-%Y')
     #date  = dt.strftime('%m-%d-%Y')
+    
+    
     db = MySQLdb.connect(host="localhost",  
                      user="root",          
                       passwd="1jyT382PWzYP",       
@@ -50,33 +52,11 @@ def jetblue(from_airport,to_airport,searchdate,searchid):
     driver.implicitly_wait(50)
     
     try:
-	'''
-        oneway = driver.find_element_by_id("jbBookerItinOW")
-        driver.execute_script("arguments[0].click();", oneway)
-        time.sleep(2)
-        origin = driver.find_element_by_id("jbBookerDepart")
-        origin.clear()
-        origin.send_keys(from_airport)
-        origin.send_keys(Keys.ARROW_DOWN)
-        origin.send_keys(Keys.ENTER)
-        destination = driver.find_element_by_id("jbBookerArrive")
-	destination.clear()
-        destination.send_keys(to_airport) 
-	time.sleep(1) 
-        destination.click() #send_keys(Keys.ARROW_DOWN)
-	time.sleep(1)
-        destination.send_keys(Keys.ENTER)
-	time.sleep(1)
-	#print "airport complete"
-        #doj = driver.find_element_by_id("jbBookerCalendarDepart")
-        #doj.send_keys(date)
-        #doj.send_keys(Keys.ENTER)
-        '''
+	    
         milestype = driver.find_elements_by_xpath("//*[contains(text(), 'TrueBlue Points')]")
-        
         driver.execute_script("arguments[0].click();", milestype[0]);
         driver.find_elements_by_css_selector("input[type='submit'][value='Find it']")[0].click()
-        time.sleep(2)
+        time.sleep(4)
         WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID, "AIR_SEARCH_RESULT_CONTEXT_ID0")))
     except:
         print "nodata on jetblue"
@@ -84,8 +64,8 @@ def jetblue(from_airport,to_airport,searchdate,searchid):
         driver.quit()
         return searchid
         
-
     try:
+        time.sleep(2)
         html_page = driver.page_source
         soup = BeautifulSoup(html_page)
         maintable = soup.find("table",{"id":"AIR_SEARCH_RESULT_CONTEXT_ID0"})
@@ -157,14 +137,17 @@ def jetblue(from_airport,to_airport,searchdate,searchid):
                         if 'With layover' in fltno:
                             fltno = fltno.replace('With layover','')
                         planetype = depttd.find("span",{"class":"equipType"}).text
-                        print fltno,planetype
+                        #print fltno,planetype
                         planeinfo = fltno+" | "+planetype
                     if arivetd:
                         arivetime = arivetd.find("div",{"class":"time"}).text
-			arival_time = arivetime
-			if '+' in arivetime:
-			    arive_time = arivetime.split("+")
-			    arivetime = arive_time[0]	
+                        arival_time = arivetime 
+                        print "arival_time",arivetime
+                        if '+' in arivetime:
+                            arive_time = arivetime.split("+")
+                            print arive_time
+                            arivetime = arive_time[0]
+                            
                         arive_fullname = arivetd.find("b").text
                         dest_code = arivetd.find("span",{"class":"location-code"}).text
                         if '(' in dest_code:
@@ -186,7 +169,8 @@ def jetblue(from_airport,to_airport,searchdate,searchid):
                             else:
                                 planetime = totaltime
                             planeinfo = planeinfo+"("+planetime+")"
-	                    plaindetails.append(planeinfo)
+
+                            plaindetails.append(planeinfo)
                        
                     if content.findAll("td",{"class":"colCost"}):
                         priceblock = content.findAll("td",{"class":"colCost"})
@@ -223,10 +207,7 @@ def jetblue(from_airport,to_airport,searchdate,searchid):
                         arivetime=''
                         
                     if flag == 0:
-                        print "ogign stn=",origin_code
-                        print "ogign time=",depttime
                         depttime1 = (datetime.datetime.strptime(depttime, '%I:%M %p'))
-                        print "depttime1",depttime1
                         depttime2 = depttime1.strftime('%H:%M')
                         arivetime1 = (datetime.datetime.strptime(arivetime, '%I:%M %p'))
                         
@@ -273,12 +254,177 @@ def jetblue(from_airport,to_airport,searchdate,searchid):
                             db.commit()
                             #transaction.commit()
                     n = n-1
-    #return searchid
+    
     except:
         print "please change your seach filter"
     display.stop
     driver.quit()
     return searchid
 
+def virginAmerica(from_airport,to_airport,searchdate,searchid):
+    db = MySQLdb.connect(host="localhost",  
+                     user="root",          
+                      passwd="1jyT382PWzYP",       
+                      db="pex")
+    cursor = db.cursor()
+    #cursor = connection.cursor()
+    from_airport = from_airport.strip()
+    to_airport = to_airport.strip()
+    dt = datetime.datetime.strptime(searchdate, '%Y/%m/%d')
+    date = dt.strftime('%m-%d-%Y')
+    searchdate = dt.strftime('%Y%m%d')
+    airport = from_airport+"_"+to_airport
+    
+    currentdatetime = datetime.datetime.now()
+    stime = currentdatetime.strftime('%Y-%m-%d %H:%M:%S')
+    url = "https://www.virginamerica.com/book/ow/a1/"+airport+"/"+str(searchdate)
+    display = Display(visible=0, size=(800, 600))
+    display.start()
+    driver = webdriver.Chrome()
+    driver.get(url)
+    try:
+        time.sleep(7)
+        milesbtn = driver.find_elements_by_name("payment_type")
+        driver.execute_script("arguments[0].click();", milesbtn[1]);
+        time.sleep(2)
+        lgn = driver.find_element_by_link_text("Close")
+        lgn.click()
+        time.sleep(4)
+        html_page = driver.page_source
+        soup = BeautifulSoup(html_page)
+        datadiv = soup.findAll("div",{"class":"fare-map-row ng-scope"})
+    except:
+        print "no flights found"
+        display.stop()
+        driver.quit()
+        return searchid
+    try:
+        for data in datadiv:
+            trblock = data.findAll("tr")
+            for content in trblock:
+                detailblock =  content.find("div",{"class":"fare-map__flight-details"})
+                deptinfo = detailblock.findAll("li",{"class":"flight-details__flight-info--dep"})
+                durationinfo = detailblock.findAll("li",{"class":"flight-details__flight-dur"})
+                ariveinfo = detailblock.findAll("li",{"class":"flight-details__flight-info--arr"})
+                flightnum = ''
+                totalduration=''
+                departfrom=''
+                arriveat = ''
+                departtime=''
+                arivetime = ''
+                stop = ''
+                deptdetail=[]
+                arivedetail=[]
+                plaindetail=[]
+                maincabin =0
+                maintax = 0
+                business = 0
+                businesstax = 0
+                First = 0
+                Firsttax = 0
+                for a in range(0,len(deptinfo)):
+                    flight_duration=''
+                    origin = deptinfo[a].find("span",{"class":"flight-info__time ng-binding"})
+                    flight = deptinfo[a].find("span",{"bo-text":"leg.flightNum"}).text
+                    flight = "Flight "+str(flight)
+                    origin_airport = origin.find("span").text
+                    origin_time = (origin.text).replace(origin_airport,'')
+                    origin_time = origin_time.strip()
+                    if '()' in  origin_time:
+                        origin_time = origin_time.replace('()','').strip()
+                    origin_time1 = (datetime.datetime.strptime(origin_time, '%I:%M%p'))
+                    dept_time = origin_time1.strftime('%H:%M')
+                    origin_airport = origin_airport.replace('(','').replace(')','')
+                    
+                    departinfo = str(date)+" | "+origin_time+" from "+origin_airport
+                    deptdetail.append(departinfo)
+                    if a < 1:
+                        departtime = dept_time
+                        departfrom = origin_airport
+                        flightnum = flight
+                    
+                    duration = durationinfo[a].findAll("span")
+                    if len(duration)>0:
+                       stop = (duration[0].text).replace('Leg','')
+                       flight_duration = duration[1].text
+                    
+                    destination = ariveinfo[a].find("span",{"class":"flight-info__time--arr ng-binding"})
+                    
+                    if ariveinfo[a].find("span",{"class":"flight-info__total ng-scope"}):
+                        totalduration = ariveinfo[a].find("span",{"class":"flight-info__total ng-scope"}).text
+                        
+                    else:
+                        totalduration = flight_duration
+                    planeinfo =  flight+"("+flight_duration+")"
+                    plaindetail.append(planeinfo)
+                    dest_airport = destination.find("span").text
+                    dest_airport = dest_airport.replace('(','').replace(')','')
+                    dest_time = (destination.text).replace(dest_airport,'')
+                    dest_time = dest_time.strip()
+                    if '()' in  dest_time:
+                        dest_time = dest_time.replace('()','').strip()
+                    dest_time1 = (datetime.datetime.strptime(dest_time, '%I:%M%p'))
+                    dest_time2 = dest_time1.strftime('%H:%M')
+                    
+                    arivetime = dest_time2
+                    arriveat = dest_airport
+                    arivalinfo = str(date)+" | "+dest_time+" at "+dest_airport
+                    arivedetail.append(arivalinfo)
+                    
+                maincabinfare = content.findAll("div",{"class":"fare-map__price-details ng-scope"})
+                count = 0
+                for fare in maincabinfare:
+                    if fare.find("a"):
+                        fareblock = (fare.find("a").text).strip()
+                        fareblock1 = re.findall("\d+.\d+", fareblock)
+                        miles = fareblock1[0]
+                        tax = fareblock1[1]
+                        if ',' in miles:
+                            miles = miles.replace(',','')
+                        if miles:
+                            if count == 0:
+                                maincabin = miles
+                                maintax = tax
+                                
+                            elif count == 1:
+                                business = miles
+                                businesstax = tax
+                                
+                            else:
+                                if count == 2:
+                                    First = miles
+                                    Firsttax = tax
+                            
+                    count = count+1
+                if 'stop' in stop:
+                    stop = "NONSTOP"
+                if (stop.strip()).isdigit():
+                    stop = int(stop)-1
+                    if int(stop) > 1:
+                        stop = stop+" STOPS"
+                    else:
+                        stop = str(stop)+" STOP"
+                    
+            
+                if 'total travel time' in totalduration:
+                    totalduration = totalduration.replace('total travel time','')
+                if maincabin > 0 or business > 0 or First > 0:
+                    departdetailtext= '@'.join(deptdetail)
+                    arivedetailtext = '@'.join(arivedetail)
+                    planedetailtext = '@'.join(plaindetail)
+                    operatortext = ''                                                                                                                                                                                                                                                                                                                     
+                    cursor.execute ("INSERT INTO pexproject_flightdata (flighno,searchkeyid,scrapetime,stoppage,stoppage_station,origin,destination,departure,arival,duration,maincabin,maintax,firstclass,firsttax,business,businesstax,cabintype1,cabintype2,cabintype3,datasource,departdetails,arivedetails,planedetails,operatedby) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", (str(flightnum), str(searchid), stime, stop, "test", departfrom, arriveat, departtime, arivetime, totalduration, str(maincabin), str(maintax), str(business),str(businesstax), str(First), str(Firsttax), "Economy", "Business", "First", "Virgin America", departdetailtext, arivedetailtext, planedetailtext, operatortext))
+                    #transaction.commit()
+                    db.commit()
+                    print "row inserted"
+                print "---------------------------- End ---------------------------"
+    except:
+        print "somethinf wrong"
+    display.stop()
+    driver.quit()
+    return searchid
+
+
 if __name__=='__main__':
-    jetblue(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+    #jetblue(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+    virginAmerica(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
