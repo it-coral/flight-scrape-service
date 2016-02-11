582a35478157e78676f89675a46901dd6c513237
it-coral <coral1212@yahoo.com>
2016-02-11 15:09:27 +0200

    changes

--- pexproject/customfunction.py
+++ pexproject/customfunction.py
@@ -12,11 +12,14 @@ from email.mime.multipart import MIMEMultipart
 from django.db import connection, transaction
 from django.core.mail import send_mail,EmailMultiAlternatives
 
-def sendMail(from_email,to_email,subject,bodytext,html_comtent=None):
+def sendMail(from_email,to_email,subject,bodytext,html_content=None):
     try:
-        mailcontent = EmailMultiAlternatives(subject,"test",from_email,[to_email])
-        mailcontent.attach_alternative(html_comtent, "text/html")
+	print "text"
+        mailcontent = EmailMultiAlternatives(subject,bodytext,from_email,[to_email])
+	if html_content:
+            mailcontent.attach_alternative(html_content, "text/html")
         mailcontent.send()
+	print "success"
         return "sent"
     except:
         return "fail"
