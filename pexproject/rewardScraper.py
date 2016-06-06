80d29d21c819a0ac52c3c912c28ff0f75ae6bc4d
it-coral <coral1212@yahoo.com>
2016-06-06 14:38:09 +0530

    fix loading popup while fetch points

--- pexproject/rewardScraper.py
+++ pexproject/rewardScraper.py
@@ -32,7 +32,7 @@ def deltaPoints(username,password,userid):
         
         driver.implicitly_wait(40)
         html_page = driver.page_source
-        soup = BeautifulSoup(html_page)
+        soup = BeautifulSoup(html_page,"lxml")
    
         WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "sky-miles")))
     except:
@@ -59,16 +59,17 @@ def unitedPoints(usernumber,password,userid):
     display.start()
     currentdatetime = datetime.datetime.now()
     stime = currentdatetime.strftime('%Y-%m-%d %H:%M:%S')
-    url = "https://www.united.com/web/en-US/apps/account/signout.aspx"
+    url = "https://www.united.com/web/en-US/apps/account/account.aspx"
     driver = webdriver.Chrome()
     driver.get(url)
-    username = driver.find_element_by_id("ctl00_ContentInfo_SignIn_onepass_txtField")
-    username.clear()
-    username.send_keys(usernumber)
-    passwrd = driver.find_element_by_id("ctl00_ContentInfo_SignIn_password_txtPassword")
-    passwrd.send_keys(password)
-    driver.find_element_by_id("ctl00_ContentInfo_SignInSecure").send_keys(Keys.ENTER)
     try:
+        username = driver.find_element_by_id("ctl00_ContentInfo_SignIn_onepass_txtField")
+        username.clear()
+        username.send_keys(usernumber)
+        passwrd = driver.find_element_by_id("ctl00_ContentInfo_SignIn_password_txtPassword")
+        passwrd.send_keys(password)
+        driver.find_element_by_id("ctl00_ContentInfo_SignInSecure").send_keys(Keys.ENTER)
+    
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ctl00_ContentInfo_AccountSummary_pnlSummaryNew"))) 
     except:
         display.stop()                                                                                                                                  
@@ -76,7 +77,7 @@ def unitedPoints(usernumber,password,userid):
         return "fail"
     driver.implicitly_wait(40)
     html_page = driver.page_source
-    soup = BeautifulSoup(html_page)
+    soup = BeautifulSoup(html_page,"lxml")
     profilediv = soup.find("div",{"id":"ctl00_ContentInfo_AccountSummary_pnlSummaryNew"})
     mileage = profilediv.find("span",{"id":"ctl00_ContentInfo_AccountSummary_lblMileageBalanceNew"})
     point = mileage.text
@@ -101,17 +102,18 @@ def virginPoints(username,password,userid):
     display.start()
     driver = webdriver.Chrome()
     driver.get(url)
-    user = driver.find_element_by_id("login_uname")
-    driver.execute_script("document.getElementById('login_uname').setAttribute('value', '"+username+"')");
-    driver.execute_script("document.getElementById('login_pwd').setAttribute('value', '"+password+"')");
-    
-    submit = driver.find_element_by_class_name("primaryAction")
-    driver.execute_script("arguments[0].click();", submit);
-    
-    driver.implicitly_wait(40)
-    html_page = driver.page_source
-    soup = BeautifulSoup(html_page)
     try:
+        user = driver.find_element_by_id("login_uname")
+        driver.execute_script("document.getElementById('login_uname').setAttribute('value', '"+username+"')");
+        driver.execute_script("document.getElementById('login_pwd').setAttribute('value', '"+password+"')");
+        
+        submit = driver.find_element_by_class_name("primaryAction")
+        driver.execute_script("arguments[0].click();", submit);
+        
+        driver.implicitly_wait(40)
+        html_page = driver.page_source
+        soup = BeautifulSoup(html_page,"lxml")
+        
         mileage = soup.find("td",{"class":"mileageValue"})
         point = (mileage.text).strip()
         cursor.execute("select * from reward_points where user_id="+str(userid)+" and airlines='virgin'")
@@ -119,7 +121,7 @@ def virginPoints(username,password,userid):
         if object:
             cursor.execute("update reward_points set reward_points="+str(point)+",update_time='"+str(stime)+"' where airlines='virgin' and user_id="+str(userid))
         else:
-            cursor.execute ("INSERT INTO reward_points (user_id,reward_points,airlines) VALUES (%s,%s,%s);", (str(userid),str(point),"virgin"))
+            cursor.execute ("INSERT INTO reward_points (user_id,reward_points,airlines,update_time) VALUES (%s,%s,%s,%s);", (str(userid),str(point),"virgin",str(stime)))
         transaction.commit()
     except:
         display.stop()                                                                                                                                  
