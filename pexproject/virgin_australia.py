d520ad86345c438c07015363281ad87494389852
it-coral <coral1212@yahoo.com>
2016-06-15 15:56:15 +0530

    add try in virgin australia

--- pexproject/virgin_australia.py
+++ pexproject/virgin_australia.py
@@ -89,152 +89,159 @@ def virginAustralia(from_airport,to_airport,searchdate,searchid,cabinName,isflag
         '''
         return searchid
     i = 1 
-    operatordiv = soup.findAll(True, {'class': re.compile(r'\operating-carrier-wrapper\b')})
-   
-    operatorArray = []
-    for div in operatordiv:
-        #print "========================================================================"
-        optrtext =''
-        optr = div.find("span",{"class":["carrier-name","operator-meta-data-internal"]})
-        if optr:
-            optrtext = optr.text
-            if '/' in optrtext[0]:
-                 optrtext = optrtext.replace('/','')
-        operatorArray.append(optrtext)
-    maindata = tempdata['dayOffers'][0]["itineraryOffers"]
-    
-    operatorDetails = []
-    value_string = []
-    operatorcounter = 0
-    for k in range(0,len(maindata)):
-        flightsDetails =[]
-        segments = maindata[k]["segments"]
-        rowRecord = maindata[k]["itineraryPartData"]
-        fltno = ''
-        #@@@@@@ Depart Details @@@@@@@@@@@@@@@@@
-        origin = rowRecord["departureCode"]
-        dest = rowRecord["arrivalCode"]
-        departureDate = rowRecord["departureDate"]
-        deptDateTime = departureDate.split(" ")
-        #departdetailFormat = departureDate+" | from "+origin
-        #originDetails.append(departdetailFormat)
-        #@@@@@@@ Segment info @@@@@@@@@@@@@@@@@
-        originDetails = []
-        destDetails = []
-        operatingCarrier = []
-        fareClassCode = []
-        for counter in range (0,len(segments)):
-            bookingCode1 = ''
-            bookingCode = segments[counter]['bookingClass']
-            if cabinType == 'E':
-                bookingCode1 = bookingCode+" Economy"
-            else:
-                bookingCode1 = bookingCode+" Business"
-            fareClassCode.append(bookingCode1)
-            segOrigin = segments[counter]["departureCode"]
-            segDepartDate = segments[counter]["departureDate"]
-            segDetailFormat = segDepartDate+" | from "+segOrigin
-            originDetails.append(segDetailFormat)
-            
-            segDest = segments[counter]["arrivalCode"]
-            segArive = segments[counter]["arrivalDate"]
-            destdetailFormat = segArive+" | at "+segDest
-            destDetails.append(destdetailFormat)
-            if len(operatorArray) >= operatorcounter:
-                operatingCarrier.append(operatorArray[operatorcounter]) 
-                operatorcounter = operatorcounter+1     
-        deptDate = deptDateTime[0]
-        depttime = deptDateTime[1]
-        depttime1 = (datetime.datetime.strptime(depttime, '%H:%M:%S'))
-        departtime = depttime1.strftime('%H:%M')
-        arrivalDate = rowRecord["arrivalDate"]
-        arrivalDateTime = arrivalDate.split(" ")
-        arivaldt = arrivalDateTime[0]
-        arivalTime = arrivalDateTime[1]
-        arivalTime1 = (datetime.datetime.strptime(arivalTime, '%H:%M:%S'))
-        arive = arivalTime1.strftime('%H:%M')
-        totalTripDuration = rowRecord["totalTripDuration"]
-        totalMinte = (int(totalTripDuration)/60000)
-        hr = totalMinte/60
-        minute = totalMinte % 60
-        tripDuration = str(hr)+"h "+str(minute)+"m"
+    try:
+        operatordiv = soup.findAll(True, {'class': re.compile(r'\operating-carrier-wrapper\b')})
+       
+        operatorArray = []
+        for div in operatordiv:
+            #print "========================================================================"
+            optrtext =''
+            optr = div.find("span",{"class":["carrier-name","operator-meta-data-internal"]})
+            if optr:
+                optrtext = optr.text
+                if '/' in optrtext[0]:
+                     optrtext = optrtext.replace('/','')
+            operatorArray.append(optrtext)
+        maindata = tempdata['dayOffers'][0]["itineraryOffers"]
         
-        departureCodes = rowRecord["departureCodes"]
-        #arrivalCodes = rowRecord["arrivalCodes"]
-        #operatingCarrier = rowRecord["operatingCarrier"]
-        flightDurations = rowRecord["flightDurations"]
-        
-        flightNumber = rowRecord["flightNumber"]
-        airlineCodes = rowRecord["airlineCodes"]
-        aircraftType = rowRecord["aircraftType"]
-        for f in range(0,len(flightNumber)):
-            flightNo = airlineCodes[f]+" "+str(flightNumber[f])
-            if f == 0:
-                fltno = flightNo
-            fltTime = flightDurations[f]
-            fltMinuteTime = int(fltTime)/60000
-            fltMinuteTimeHour = fltMinuteTime/60
-            fltMinuteTime = fltMinuteTime % 60
-            fltTimeFormat = str(fltMinuteTimeHour)+"h "+str(fltMinuteTime)+"m"
-            fltFormat = flightNo+" | "+aircraftType[f]+" ("+fltTimeFormat+")"
-            flightsDetails.append(fltFormat)
-        originDetailString = '@'.join(originDetails)
-        arivedetailtext = '@'.join(destDetails)
-        planedetailtext = '@'.join(flightsDetails)
-        ecoFareCode = ''
-        busFareCode = ''
-        firstFareCode = ''
-        if len(fareClassCode)> 0:
-            if cabinType == 'E':
-                ecoFareCode = '@'.join(fareClassCode)
-            else:
-                busFareCode = '@'.join(fareClassCode)
-
-        noOfStop = len(departureCodes) - 1
-        stoppage = ''
-        if noOfStop == 0:
-            stoppage = "NONSTOP"
-        elif noOfStop == 1:
-            stoppage = "1 STOP"
-        else:
-            stoppage = str(noOfStop)+" STOPS"
-        if len(operatingCarrier) > 0:
-            operatortext = '@'.join(operatingCarrier)
-        allPrices = maindata[k]["basketsRef"]
-        for key in allPrices:
-            farePrices = maindata[k]["basketsRef"][key]["prices"]["priceAlternatives"]
-            economy = 0
-            ecotax = 0
-            business = 0
-            businesstax = 0
-            first = 0
-            firsttax = 0
-            for m in range(0,len(farePrices)):
-                saverPrice = farePrices[m]["pricesPerCurrency"]
-                taxes = 0
-                miles = 0
-                if "AUD" in saverPrice:
-                    taxes = saverPrice["AUD"]["amount"]
-                miles = saverPrice["FFCURRENCY"]["amount"]
+        operatorDetails = []
+        value_string = []
+        operatorcounter = 0
+        for k in range(0,len(maindata)):
+            flightsDetails =[]
+            segments = maindata[k]["segments"]
+            rowRecord = maindata[k]["itineraryPartData"]
+            fltno = ''
+            #@@@@@@ Depart Details @@@@@@@@@@@@@@@@@
+            origin = rowRecord["departureCode"]
+            dest = rowRecord["arrivalCode"]
+            departureDate = rowRecord["departureDate"]
+            deptDateTime = departureDate.split(" ")
+            #departdetailFormat = departureDate+" | from "+origin
+            #originDetails.append(departdetailFormat)
+            #@@@@@@@ Segment info @@@@@@@@@@@@@@@@@
+            originDetails = []
+            destDetails = []
+            operatingCarrier = []
+            fareClassCode = []
+            for counter in range (0,len(segments)):
+                bookingCode1 = ''
+                bookingCode = segments[counter]['bookingClass']
                 if cabinType == 'E':
-                    economy = miles
-                    ecotax = taxes
+                    bookingCode1 = bookingCode+" Economy"
                 else:
-                    business = miles
-                    businesstax = taxes
-                value_string.append((str(fltno), str(searchid), stime, stoppage, "test", origin, dest, departtime, arive, tripDuration, str(economy), str(ecotax), str(business),str(businesstax), str(first), str(firsttax), "Economy", "Business", "First", "Virgin Australia", originDetailString, arivedetailtext, planedetailtext, operatortext,ecoFareCode,busFareCode,firstFareCode))
-                if len(value_string) == 50:
-                    cursor.executemany ("INSERT INTO pexproject_flightdata (flighno,searchkeyid,scrapetime,stoppage,stoppage_station,origin,destination,departure,arival,duration,maincabin,maintax,firstclass,firsttax,business,businesstax,cabintype1,cabintype2,cabintype3,datasource,departdetails,arivedetails,planedetails,operatedby,economy_code,business_code,first_code) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", value_string)
-                    db.commit()
-                    value_string=[]
-    if len(value_string) > 0:
-        cursor.executemany ("INSERT INTO pexproject_flightdata (flighno,searchkeyid,scrapetime,stoppage,stoppage_station,origin,destination,departure,arival,duration,maincabin,maintax,firstclass,firsttax,business,businesstax,cabintype1,cabintype2,cabintype3,datasource,departdetails,arivedetails,planedetails,operatedby,economy_code,business_code,first_code) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", value_string)
-        db.commit()
-    if isflag:   
-        cursor.execute ("INSERT INTO pexproject_flightdata (flighno,searchkeyid,scrapetime,stoppage,stoppage_station,origin,destination,duration,maincabin,maintax,firstclass,firsttax,business,businesstax,cabintype1,cabintype2,cabintype3,datasource,departdetails,arivedetails,planedetails,operatedby,economy_code,business_code,first_code) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", ("flag", str(searchid), stime, "flag", "test", "flag", "flag", "flag", "0","0", "0","0", "0", "0", "flag", "flag", "flag", "Virgin Australia", "flag", "flag", "flag", "flag", "flag", "flag", "flag"))
-        db.commit()
-    display.stop()
-    driver.quit()
+                    bookingCode1 = bookingCode+" Business"
+                fareClassCode.append(bookingCode1)
+                segOrigin = segments[counter]["departureCode"]
+                segDepartDate = segments[counter]["departureDate"]
+                segDetailFormat = segDepartDate+" | from "+segOrigin
+                originDetails.append(segDetailFormat)
+                
+                segDest = segments[counter]["arrivalCode"]
+                segArive = segments[counter]["arrivalDate"]
+                destdetailFormat = segArive+" | at "+segDest
+                destDetails.append(destdetailFormat)
+                if len(operatorArray) >= operatorcounter:
+                    operatingCarrier.append(operatorArray[operatorcounter]) 
+                    operatorcounter = operatorcounter+1     
+            deptDate = deptDateTime[0]
+            depttime = deptDateTime[1]
+            depttime1 = (datetime.datetime.strptime(depttime, '%H:%M:%S'))
+            departtime = depttime1.strftime('%H:%M')
+            arrivalDate = rowRecord["arrivalDate"]
+            arrivalDateTime = arrivalDate.split(" ")
+            arivaldt = arrivalDateTime[0]
+            arivalTime = arrivalDateTime[1]
+            arivalTime1 = (datetime.datetime.strptime(arivalTime, '%H:%M:%S'))
+            arive = arivalTime1.strftime('%H:%M')
+            totalTripDuration = rowRecord["totalTripDuration"]
+            totalMinte = (int(totalTripDuration)/60000)
+            hr = totalMinte/60
+            minute = totalMinte % 60
+            tripDuration = str(hr)+"h "+str(minute)+"m"
+            
+            departureCodes = rowRecord["departureCodes"]
+            #arrivalCodes = rowRecord["arrivalCodes"]
+            #operatingCarrier = rowRecord["operatingCarrier"]
+            flightDurations = rowRecord["flightDurations"]
+            
+            flightNumber = rowRecord["flightNumber"]
+            airlineCodes = rowRecord["airlineCodes"]
+            aircraftType = rowRecord["aircraftType"]
+            for f in range(0,len(flightNumber)):
+                flightNo = airlineCodes[f]+" "+str(flightNumber[f])
+                if f == 0:
+                    fltno = flightNo
+                fltTime = flightDurations[f]
+                fltMinuteTime = int(fltTime)/60000
+                fltMinuteTimeHour = fltMinuteTime/60
+                fltMinuteTime = fltMinuteTime % 60
+                fltTimeFormat = str(fltMinuteTimeHour)+"h "+str(fltMinuteTime)+"m"
+                fltFormat = flightNo+" | "+aircraftType[f]+" ("+fltTimeFormat+")"
+                flightsDetails.append(fltFormat)
+            originDetailString = '@'.join(originDetails)
+            arivedetailtext = '@'.join(destDetails)
+            planedetailtext = '@'.join(flightsDetails)
+            ecoFareCode = ''
+            busFareCode = ''
+            firstFareCode = ''
+            if len(fareClassCode)> 0:
+                if cabinType == 'E':
+                    ecoFareCode = '@'.join(fareClassCode)
+                else:
+                    busFareCode = '@'.join(fareClassCode)
+    
+            noOfStop = len(departureCodes) - 1
+            stoppage = ''
+            if noOfStop == 0:
+                stoppage = "NONSTOP"
+            elif noOfStop == 1:
+                stoppage = "1 STOP"
+            else:
+                stoppage = str(noOfStop)+" STOPS"
+            if len(operatingCarrier) > 0:
+                operatortext = '@'.join(operatingCarrier)
+            allPrices = maindata[k]["basketsRef"]
+            for key in allPrices:
+                farePrices = maindata[k]["basketsRef"][key]["prices"]["priceAlternatives"]
+                economy = 0
+                ecotax = 0
+                business = 0
+                businesstax = 0
+                first = 0
+                firsttax = 0
+                for m in range(0,len(farePrices)):
+                    saverPrice = farePrices[m]["pricesPerCurrency"]
+                    taxes = 0
+                    miles = 0
+                    if "AUD" in saverPrice:
+                        taxes = saverPrice["AUD"]["amount"]
+                    miles = saverPrice["FFCURRENCY"]["amount"]
+                    if cabinType == 'E':
+                        economy = miles
+                        ecotax = taxes
+                    else:
+                        business = miles
+                        businesstax = taxes
+                    value_string.append((str(fltno), str(searchid), stime, stoppage, "test", origin, dest, departtime, arive, tripDuration, str(economy), str(ecotax), str(business),str(businesstax), str(first), str(firsttax), "Economy", "Business", "First", "Virgin Australia", originDetailString, arivedetailtext, planedetailtext, operatortext,ecoFareCode,busFareCode,firstFareCode))
+                    if len(value_string) == 50:
+                        cursor.executemany ("INSERT INTO pexproject_flightdata (flighno,searchkeyid,scrapetime,stoppage,stoppage_station,origin,destination,departure,arival,duration,maincabin,maintax,firstclass,firsttax,business,businesstax,cabintype1,cabintype2,cabintype3,datasource,departdetails,arivedetails,planedetails,operatedby,economy_code,business_code,first_code) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", value_string)
+                        db.commit()
+                        value_string=[]
+        if len(value_string) > 0:
+            cursor.executemany ("INSERT INTO pexproject_flightdata (flighno,searchkeyid,scrapetime,stoppage,stoppage_station,origin,destination,departure,arival,duration,maincabin,maintax,firstclass,firsttax,business,businesstax,cabintype1,cabintype2,cabintype3,datasource,departdetails,arivedetails,planedetails,operatedby,economy_code,business_code,first_code) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", value_string)
+            db.commit()
+        if isflag:   
+            cursor.execute ("INSERT INTO pexproject_flightdata (flighno,searchkeyid,scrapetime,stoppage,stoppage_station,origin,destination,duration,maincabin,maintax,firstclass,firsttax,business,businesstax,cabintype1,cabintype2,cabintype3,datasource,departdetails,arivedetails,planedetails,operatedby,economy_code,business_code,first_code) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", ("flag", str(searchid), stime, "flag", "test", "flag", "flag", "flag", "0","0", "0","0", "0", "0", "flag", "flag", "flag", "Virgin Australia", "flag", "flag", "flag", "flag", "flag", "flag", "flag"))
+            db.commit()
+        display.stop()
+        driver.quit()
+    except:
+        if isflag:   
+            cursor.execute ("INSERT INTO pexproject_flightdata (flighno,searchkeyid,scrapetime,stoppage,stoppage_station,origin,destination,duration,maincabin,maintax,firstclass,firsttax,business,businesstax,cabintype1,cabintype2,cabintype3,datasource,departdetails,arivedetails,planedetails,operatedby,economy_code,business_code,first_code) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", ("flag", str(searchid), stime, "flag", "test", "flag", "flag", "flag", "0","0", "0","0", "0", "0", "flag", "flag", "flag", "Virgin Australia", "flag", "flag", "flag", "flag", "flag", "flag", "flag"))
+            db.commit()
+        display.stop()
+        driver.quit()
     return searchid
 
 
