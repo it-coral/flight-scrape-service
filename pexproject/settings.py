706ec5d86034d8f626c085db1394e9b812fd4677
it-coral <coral1212@yahoo.com>
2016-07-12 01:25:17 -0500

    initial hotel api version, model sync

--- pexproject/settings.py
+++ pexproject/settings.py
@@ -84,21 +84,14 @@ WSGI_APPLICATION = 'pexproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'pex',
-#         'USER': 'root',
-#         'PASSWORD': '1jyT382PWzYP',
-#         'default-character-set' : 'utf8',
-#         'host': 'localhost',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pex',
+        'USER': 'root',
+        'PASSWORD': '1jyT382PWzYP',
+        'default-character-set' : 'utf8',
+        'host': 'localhost',
     }
 }
 
