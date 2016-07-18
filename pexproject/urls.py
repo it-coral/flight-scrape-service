b9a501c12b1136367e01ab2ed510f4d1a855284d
it-coral <coral1212@yahoo.com>
2016-07-18 18:10:18 -0500

    admin initial version

--- pexproject/urls.py
+++ pexproject/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf.urls import handler404
-#from django.contrib import admin
+from django.contrib import admin
 
 
 
