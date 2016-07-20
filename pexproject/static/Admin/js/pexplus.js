e6debfbd884a9c4afe2e272cc222d0e83960f645
it-coral <coral1212@yahoo.com>
2016-07-20 11:04:52 -0500

    popular searhces

--- pexproject/static/Admin/js/pexplus.js
+++ pexproject/static/Admin/js/pexplus.js
@@ -6,8 +6,8 @@ update_pop_search = function(obj) {
     $.post('/stats/popular_search/', 
         {'period':period}
     ).success(function(data) {
+        console.log(data);
         pop_searches = JSON.parse(data);
-        console.log(pop_searches);
         var result = '';
         for(idx in pop_searches) {
             var rank = idx*1 + 1;
