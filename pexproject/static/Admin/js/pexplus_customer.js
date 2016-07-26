ec25929d595186e39c96aa859b3455fd504eb80e
it-coral <coral1212@yahoo.com>
2016-07-26 03:15:48 -0500

    customer analytics airline info

--- pexproject/static/Admin/js/pexplus_customer.js
+++ pexproject/static/Admin/js/pexplus_customer.js
@@ -110,15 +110,12 @@ _update_line_info = function(data) {
 update_line_info = function(obj) {
     var period = $('#id_airline_info_period').val();
     var fare_class = $('#id_airline_info_fare_class').val();
-    var _from = $('#id_search_result_from').val();
-    var _to = $('#id_search_result_to').val();
-    
-    if (_from == '' || _to == '')
-        return false;
+    var _from = $('#id_airline_info_from').val();
+    var _to = $('#id_airline_info_to').val();
     
     $('.page-loader').show();    
 
-    $.post('/customer/stats/airline_info/', 
+    $.post('/stats/airline_info/', 
         {'period':period, 'fare_class':fare_class, '_from':_from, '_to':_to}
     ).success(function(data) {
         stat_num_search = JSON.parse(data);
