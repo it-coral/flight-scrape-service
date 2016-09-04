253e3f9ce157119255ece433b32b709def5c9e69
it-coral <coral1212@yahoo.com>
2016-09-04 18:18:03 -0500

    zoombie manager revise

--- pexproject/scraper_manager.py
+++ pexproject/scraper_manager.py
@@ -9,14 +9,15 @@ def kill_children(proc):
 
 
 for proc in psutil.process_iter():
-    names = ['Xvfb', 'chromedriver']
+    names = ['Xvfb', 'chromedriver', 'phantomjs']
     # current time in seconds
     current_time = time.time()
 
     try:
         pinfo = proc.as_dict(attrs=['pid', 'ppid', 'name', 'create_time', 'username', 'cwd'])
 
-        if pinfo['username'] == 'www-data' and pinfo['name'] in names:
+        # if pinfo['username'] == 'www-data' and pinfo['name'] in names:
+        if pinfo['name'] in names:
             # elapsed time in minutes
             pinfo['create_time'] = int((current_time - pinfo['create_time']))
             # if orphan process
