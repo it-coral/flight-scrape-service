82179b97d5241caeba8cc2c0160a1a16a6e75724
it-coral <coral1212@yahoo.com>
2016-09-07 14:51:43 -0500

    flight homepage revise

--- pexproject/urls.py
+++ pexproject/urls.py
@@ -10,6 +10,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='flightsearch/home.html'), name='index'),
+    url(r'^index$', TemplateView.as_view(template_name='flightsearch/home.html'), name='index'),
     url(r'^flights', TemplateView.as_view(template_name='flightsearch/home.html'), name='flights'),
     url(r'^destination_tiles$', destination_tiles, name='destination_tiles'),
     url(r'', include('social_auth.urls')),
