6a265fd70e9009f0c3aa297cad633e58cf069f4f
it-coral <coral1212@yahoo.com>
2016-09-17 10:25:02 -0500

    code optimize and organize

--- pexproject/cron_job/refresh_rate_limit.py
+++ pexproject/cron_job/refresh_rate_limit.py
@@ -1,5 +1,5 @@
 from os import sys, path
-sys.path.append(path.dirname(path.abspath(__file__)))
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from scrapers import customfunction 
 
 db = customfunction.dbconnection()
