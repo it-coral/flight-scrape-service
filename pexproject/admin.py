bd067d19296668e3032d01065d1b0f2218e9f04d
it-coral <coral1212@yahoo.com>
2016-09-17 19:04:09 -0500

    user level management

--- pexproject/admin.py
+++ pexproject/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from models import User
-admin.site.register(User)
+
+class ClientAdmin(UserAdmin):
+	fieldsets = None
+	fields = ['username', 'password', 'is_active', 'is_staff', 'is_superuser', 
+			  'first_name', 'middlename', 'last_name', 'email', 'groups', 
+			  'gender', 'date_of_birth', 'language', 'country', 'phone', 
+			  'home_airport', 'address1', 'address2', 'city', 'state',
+			  'zipcode', 'usercode', 'user_code_time', 'pexdeals', 'level',
+			  'search_limit', 'search_run']
+
+admin.site.register(User, ClientAdmin)
