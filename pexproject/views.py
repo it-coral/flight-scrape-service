385674193305018bfbb7225cfbc15d5acbc1cb06
it-coral <coral1212@yahoo.com>
2016-10-29 10:34:26 -0500

    fix crazy error in index()

--- pexproject/views.py
+++ pexproject/views.py
@@ -178,13 +178,13 @@ def index(request):
         user = User.objects.filter(username=request.user)
         if len(user) > 0:
             user = User.objects.get(username=request.user)
-        request.session['username'] = request.user.username
-        if user.first_name != '':
-            request.session['first_name'] = user.first_name
-        if user.home_airport != '':
-            request.session['homeairpot'] = user.home_airport
-        request.session['userid'] = user.user_id
-        request.session['level'] = user.level
+            request.session['username'] = request.user.username
+            if user.first_name != '':
+                request.session['first_name'] = user.first_name
+            if user.home_airport != '':
+                request.session['homeairpot'] = user.home_airport
+            request.session['userid'] = user.user_id
+            request.session['level'] = user.level
     else:
         email=request.user
         password = ''
@@ -196,14 +196,14 @@ def index(request):
         object = User(username=email,email=email, password=password1,first_name=first_name,last_name=last_name, home_airport=airport,pexdeals=pexdeals, last_login=dttime.now())
         object.save()
         user = User.objects.get(username=request.user)
-
-        request.session['username'] = request.user.username
-        if user.first_name != '':
-            request.session['first_name'] = user.first_name
-        if user.home_airport != '':
-            request.session['homeairpot'] = user.home_airport
-        request.session['userid'] = user.user_id
-        request.session['level'] = user.level
+        if len(user) > 0:
+            request.session['username'] = request.user.username
+            if user.first_name != '':
+                request.session['first_name'] = user.first_name
+            if user.home_airport != '':
+                request.session['homeairpot'] = user.home_airport
+            request.session['userid'] = user.user_id
+            request.session['level'] = user.level
 
     return render(request, 'flightsearch/home.html')    
 
