74f14a37aa279b619d305fb476efc75f6300139e
it-coral <coral1212@yahoo.com>
2016-10-30 21:06:44 -0500

    aircraft parent checkbox debug

--- pexproject/static/flightsearch/js/searchresult.js
+++ pexproject/static/flightsearch/js/searchresult.js
@@ -1060,9 +1060,9 @@ function filter_aircraft_dropdown(this_) {
 
 function filter_aircraft_check(this_) {
     var checked = $(this_).prop('checked');
-    console.log($(this_).parent().parent().parent());    
+    console.log($(this_).parent().parent().parent().children('.filter-aircraft-body'));    
     
-    $(this_).parent().parent().children('.filter-aircraft-body').each(function() {
+    $(this_).parent().parent().parent().children('.filter-aircraft-body').each(function() {
         console.log($(this));    
         $(this).children('input').prop('checked', checked);
     });
