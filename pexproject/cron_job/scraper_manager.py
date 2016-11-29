1509e5d9c68654c0ef57678431b13f1b97ffc63e
it-coral <coral1212@yahoo.com>
2016-11-29 14:53:03 -0600

    fix scraper manager

--- pexproject/cron_job/scraper_manager.py
+++ pexproject/cron_job/scraper_manager.py
@@ -9,23 +9,23 @@ def kill_children(proc):
 
 
 for proc in psutil.process_iter():
-    names = ['Xvfb', 'chromedriver', 'phantomjs']
-    # current time in seconds
+    # names = ['Xvfb', 'chromedriver', 'phantomjs']
+    names = ['node', 'phantomjs']
+    """ current time in seconds """
     current_time = time.time()
 
     try:
         pinfo = proc.as_dict(attrs=['pid', 'ppid', 'name', 'create_time', 'username', 'cwd'])
 
+        """ if orphan process """
         # if pinfo['username'] == 'www-data' and pinfo['name'] in names:
-        if pinfo['name'] in names:
-            # if orphan process
-            if pinfo['ppid'] == 1:
-                kill_children(proc)
-        # for long-running processes
-        elif pinfo['name'] == 'python' and pinfo['username'] == 'www-data':
+        if pinfo['name'] in names and pinfo['ppid'] == 1:
+            kill_children(proc)
+        elif pinfo['name'] == 'python' and pinfo['username'] in ['www-data', 'root'] and pinfo['ppid'] != 1:
+            """ for long-running processes """
             # elapsed time in seconds
             duration = int((current_time - pinfo['create_time']))
-            if duration > 300:
+            if duration > 60 * 3:
                 kill_children(proc)
 
     except psutil.NoSuchProcess:
