74c380eac4846739152cdbe9e542030bc22a01d7
it-coral <coral1212@yahoo.com>
2016-12-03 21:07:40 -0600

    alaska debugging

--- pexproject/scrapers/alaska.py
+++ pexproject/scrapers/alaska.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 DEV_LOCAL = False
-# DEV_LOCAL = True
+DEV_LOCAL = True
 
 if not DEV_LOCAL:
     import customfunction
@@ -150,11 +150,11 @@ def alaska(ocity_code, dcity_code, searchdate, searchkey):
                 duration_ = get_clean_string(DetailsSmall[1].select('ul li')[0].text).replace('Duration: ', '').replace('hours', 'h ').replace('minutes', 'm')
 
                 flightno.append(flightno_)
-                origin_ = customfunction.get_airport_detail(customfunction.get_airport_code(departinfo_airport)) or origin_
+                # origin_ = customfunction.get_airport_detail(customfunction.get_airport_code(departinfo_airport)) or origin_
                 departure.append(departure_+' | from '+origin_)
                 departure_t.append(departure_t_)
                 
-                destination_ = customfunction.get_airport_detail(customfunction.get_airport_code(departinfo_airport)) or destination_
+                # destination_ = customfunction.get_airport_detail(customfunction.get_airport_code(departinfo_airport)) or destination_
                 arrival.append(arrival_+' | at '+destination_)
                 arrival_t.append(arrival_t_)
                 planeinfo.append("{} | {} ({})".format(flightno_, planeinfo_, duration_))
@@ -183,7 +183,7 @@ def alaska(ocity_code, dcity_code, searchdate, searchkey):
             if DEV_LOCAL:
                 print (flightno[0], str(searchkey), stime, stoppage, "test", flight['orig'], flight['dest'], departure_t[0], arrival_t[-1], total_duration, main_mile, main_tax, business_mile, business_tax, first_mile, first_tax,"Economy", "Business", "First", "alaska", departinfo, arrivalinfo, planeinfo, operatedby,'X Economy','I Business','O First','X','I','O')
     except Exception, e:
-        # raise
+        raise
         print 'Something is wrong'
 
 
