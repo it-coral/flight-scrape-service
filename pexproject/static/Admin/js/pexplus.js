3d6ce98cc378ee2d04a9281e0119e1377db55b84
it-coral <coral1212@yahoo.com>
2016-12-20 00:03:27 -0600

    add tooltip for graph

--- pexproject/static/Admin/js/pexplus.js
+++ pexproject/static/Admin/js/pexplus.js
@@ -305,6 +305,20 @@ search_avg = function() {
                 mode: "time"
             }
         });  
+
+        $("#id_search_avg_chart").bind("plothover", function (event, pos, item) {
+            if (item) {
+                var x = item.datapoint[0].toFixed(2),
+                    y = item.datapoint[1].toFixed(2);
+                $(".flot-tooltip").html(item.series.label + " of " + x + " = " + y).css({top: item.pageY+5, left: item.pageX+5}).show();
+            }
+            else {
+                $(".flot-tooltip").hide();
+            }
+        });
+        
+        $("<div class='flot-tooltip' class='chart-tooltip'></div>").appendTo("body");
+        
     });
 }
 
